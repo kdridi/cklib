98b66ad11a118f071e06d1911c6f82b9f201fe60
Karim DRIDI <karim.dridi@gmail.com>
2017-02-23 18:12:57 +0100

    cpp compilation error

--- include/ck/ck.h
+++ include/ck/ck.h
@@ -20,13 +20,13 @@ extern "C" {
 
 void *  ck_malloc  (size_t size);
 void    ck_free    (void *ptr);
-void *  ck_memcpy  (void *restrict dst, const void *restrict src, size_t n);
+void *  ck_memcpy  (void *dst, const void *src, size_t n);
 void *  ck_memmove (void *dst, const void *src, size_t len);
 size_t  ck_strlen  (const char *s);
 char *  ck_strdup  (const char *s1);
 ssize_t ck_read    (int fildes, void *buf, size_t nbyte);
-int     ck_printf  (const char * restrict format, ...);
-int     ck_vprintf (const char * restrict format, va_list ap);
+int     ck_printf  (const char * format, ...);
+int     ck_vprintf (const char * format, va_list ap);
 
 #ifdef __cplusplus
 }
